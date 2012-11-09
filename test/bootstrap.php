cb7158474eabe4d15b9f03908139015ff6af3b32
Brian Scaturro <scaturrob@gmail.com>
2012-11-08 19:40:31 -0500

    defer autoloading to composer

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,30 +4,6 @@ if(!defined('DS'))
 
 require_once dirname(__DIR__) . DS . 'vendor' . DS . 'autoload.php';
 
-//PSR-0 autoloader modified to account for test and src dirs
-function autoload($className)
-{
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strripos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DS, $namespace) . DS;
-    }
-    $fileName .= str_replace('_', DS, $className) . '.php';
-
-    $srcFile = 'src' . DS . $fileName;
-    $testFile = 'test' . DS . $fileName;
-    $integrationFile = 'it' . DS . $fileName;
-
-    if(file_exists($srcFile)) require $srcFile;
-    if(file_exists($testFile)) require $testFile;
-    if(file_exists($integrationFile)) require $integrationFile;
-}
-
-spl_autoload_register('autoload');
-
 //TEST CONSTANTS
 define('FIXTURES', __DIR__ . DS . 'fixtures');
 define("PARA_BINARY", dirname(dirname(__FILE__)) . DS . 'bin' . DS . 'paratest');
