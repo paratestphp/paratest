ad95078d45f8790f00f695cd3045d04d88bc9fbf
Brian Scaturro <scaturrob@gmail.com>
2012-11-08 19:57:18 -0500

    clean up bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -8,11 +8,13 @@ require_once dirname(__DIR__) . DS . 'vendor' . DS . 'autoload.php';
 define('FIXTURES', __DIR__ . DS . 'fixtures');
 define("PARA_BINARY", dirname(dirname(__FILE__)) . DS . 'bin' . DS . 'paratest');
 define("PARATEST_ROOT", dirname(__DIR__));
+
 //check for .bat first if on windows.
 $phpunit_path = PARATEST_ROOT . DS . 'vendor' . DS . 'bin' . DS . 'phpunit';
 if(file_exists($phpunit_path . '.bat'))
     $phpunit_path = $phpunit_path . '.bat';
 define("PHPUNIT", $phpunit_path);
+
 define('BOOTSTRAP', __FILE__);
 
 require_once __DIR__ . DS . 'TestBase.php';
