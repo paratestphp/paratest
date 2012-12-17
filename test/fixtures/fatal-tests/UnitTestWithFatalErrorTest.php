3b5a80b52e4a49c6c1034da47c83e88e23760fb8
Brian Scaturro <scaturrob@gmail.com>
2012-12-17 07:02:27 -0500

    verify fatal parse error will retrun 255 exit code

---
+++ test/fixtures/fatal-tests/UnitTestWithFatalErrorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'UnitTestWithMethodAnnotationsTest.php';
+
+/**
+ * @runParallel
+ */
+class UnitTestWithFatalErrorTest extends UnitTestWithMethodAnnotationsTest
+{
+    /**
+     * @group fixtures
+     */
+    public function testTruth()
+    {
+        I will fail fataly because this is not a php statement.
+    }
+
+    /**
+     * @test
+     */
+    public function isItFalse()
+    {
+        sleep(2);
+        $this->assertFalse(false);
+    }
+}
