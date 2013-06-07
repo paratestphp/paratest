0ce911fa9532ab522bd6e1824413c1fa20077730
Brian Scaturro <bscaturro@ostusa.com>
2013-06-06 21:03:07 -0400

    remove command string test for env vars

--- test/ParaTest/Runners/PHPUnit/ExecutableTestTest.php
+++ test/ParaTest/Runners/PHPUnit/ExecutableTestTest.php
@@ -29,16 +29,6 @@ class ExecutableTestTest extends \TestBase
         $this->assertEquals('/usr/bin/phpunit --bootstrap test/bootstrap.php pathToFile', $command);
     }
 
-    /*public function testGetCommandStringIncludesEnvironmentVariables()
-    {
-        $options = array('bootstrap' => 'test/bootstrap.php');
-        $binary = '/usr/bin/phpunit';
-        $environmentVariables = array('TEST_TOKEN' => 3, 'APPLICATION_ENVIRONMENT_VAR' => 'abc');
-        $command = $this->call($this->executableTestChild, 'getCommandString', $binary, $options, $environmentVariables);
-
-        $this->assertEquals('TEST_TOKEN=3 APPLICATION_ENVIRONMENT_VAR=abc /usr/bin/phpunit --bootstrap test/bootstrap.php pathToFile', $command);
-    }*/
-
     public function testHandleEnvironmentVariablesAssignsToken()
     {
         $environmentVariables = array('TEST_TOKEN' => 3, 'APPLICATION_ENVIRONMENT_VAR' => 'abc');
