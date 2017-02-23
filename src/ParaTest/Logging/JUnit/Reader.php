a65b6bc599bbc1ec7aada56085d464abe79a29b2
Thomas Baumann <baumann@united-domains.de>
2017-02-23 09:33:57 +0100

    Removed method, as it is not used anymore.

--- src/ParaTest/Logging/JUnit/Reader.php
+++ src/ParaTest/Logging/JUnit/Reader.php
@@ -74,17 +74,6 @@ class Reader extends MetaProvider
     }
 
     /**
-     * Checks whether the xml log has
-     * test suite results
-     *
-     * @return array
-     */
-    public function hasResults()
-    {
-        return $this->xml->count() > 0;
-    }
-
-    /**
      * Return an array that contains
      * each suite's instant feedback. Since
      * logs do not contain skipped or incomplete
