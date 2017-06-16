700290884a48a09673a84f8a7c62fb5819826886
Julian Seeger <seeger.julian@gmail.com>
2017-06-16 22:07:10 +0200

    removed genrated file header
    with wrong license and copyright info

--- test/functional/EmptyRunnerStub.php
+++ test/functional/EmptyRunnerStub.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * EmptyRunnerStub.php
- *
- * @package    Atlas
- * @author     Christopher Biel <christopher.biel@jungheinrich.de>
- * @copyright  2013 Jungheinrich AG
- * @license    Proprietary license
- * @version    $Id$
- */
+
 class EmptyRunnerStub extends \ParaTest\Runners\PHPUnit\BaseRunner
 {
     public function run()
