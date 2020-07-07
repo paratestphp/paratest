0a85904b6b2a895950f2e0b5ed1e354a39bc8310
js-moreno <66801799+js-moreno@users.noreply.github.com>
2020-07-07 02:03:51 -0500

    fix: error exception coverage error
    #447

--- src/Coverage/CoverageMerger.php
+++ src/Coverage/CoverageMerger.php
@@ -84,7 +84,9 @@ class CoverageMerger
 
         $this->addCoverage($this->getCoverageObject($file));
 
-        \unlink($file->getRealPath());
+        unset($file);
+
+        \unlink($coverageFile);
     }
 
     /**
