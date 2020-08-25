61f3964b205c630408b138bdc946ceb2a0f9238b
Filippo Tessarotto <zoeslam@gmail.com>
2020-08-25 09:45:26 +0200

    Fix CC

--- test/Unit/Logging/JUnit/ReaderTest.php
+++ test/Unit/Logging/JUnit/ReaderTest.php
@@ -233,9 +233,10 @@ final class ReaderTest extends TestBase
     {
         static::assertSame(19, $this->mixed->getTotalTests());
         static::assertSame(10, $this->mixed->getTotalAssertions());
+        static::assertSame(3, $this->mixed->getTotalErrors());
         static::assertSame(3, $this->mixed->getTotalFailures());
         static::assertSame(2, $this->mixed->getTotalWarnings());
-        static::assertSame(3, $this->mixed->getTotalErrors());
+        static::assertSame(4, $this->mixed->getTotalSkipped());
         static::assertSame(0.001489, $this->mixed->getTotalTime());
     }
 
@@ -243,9 +244,10 @@ final class ReaderTest extends TestBase
     {
         static::assertSame(3, $this->single->getTotalTests());
         static::assertSame(3, $this->single->getTotalAssertions());
+        static::assertSame(0, $this->single->getTotalErrors());
         static::assertSame(1, $this->single->getTotalFailures());
         static::assertSame(0, $this->single->getTotalWarnings());
-        static::assertSame(0, $this->single->getTotalErrors());
+        static::assertSame(4, $this->mixed->getTotalSkipped());
         static::assertSame(0.005895, $this->single->getTotalTime());
     }
 
