d6ea3d52bba1ba1b8bee02a107a9cd8dd15b77f0
Mikhail Fesenko <proggga@gmail.com>
2020-10-15 00:23:07 +0300

    Fix phpunit-wrapper from resetting variables in global scope in WrapperRunner

--- bin/phpunit-wrapper.php
+++ bin/phpunit-wrapper.php
@@ -2,39 +2,41 @@
 
 declare(strict_types=1);
 
-$opts = getopt('', [
-    'write-to:',
-]);
+function phpunitWrapperMain(): void {
 
-$composerAutoloadFiles = [
+  $opts = getopt('', [
+    'write-to:',
+  ]);
+
+  $composerAutoloadFiles = [
     dirname(__DIR__, 3) . DIRECTORY_SEPARATOR . 'autoload.php',
     dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
     dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
-];
+  ];
 
-foreach ($composerAutoloadFiles as $file) {
+  foreach ($composerAutoloadFiles as $file) {
     if (file_exists($file)) {
-        require_once $file;
-        define('PHPUNIT_COMPOSER_INSTALL', $file);
+      require_once $file;
+      define('PHPUNIT_COMPOSER_INSTALL', $file);
 
-        break;
+      break;
     }
-}
+  }
 
-assert(is_string($opts['write-to']));
-$writeTo = fopen($opts['write-to'], 'wb');
-assert(is_resource($writeTo));
+  assert(is_string($opts['write-to']));
+  $writeTo = fopen($opts['write-to'], 'wb');
+  assert(is_resource($writeTo));
 
-$i = 0;
-while (true) {
+  $i = 0;
+  while (true) {
     $i++;
     if (feof(\STDIN)) {
-        exit;
+      exit;
     }
 
     $command = fgets(\STDIN);
     if ($command === false || $command === \ParaTest\Runners\PHPUnit\Worker\WrapperWorker::COMMAND_EXIT) {
-        exit;
+      exit;
     }
 
     $arguments = unserialize($command);
@@ -42,4 +44,7 @@ while (true) {
 
     fwrite($writeTo, \ParaTest\Runners\PHPUnit\Worker\WrapperWorker::TEST_EXECUTED_MARKER);
     fflush($writeTo);
+  }
 }
+
+phpunitWrapperMain();
