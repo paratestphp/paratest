03ee8efb2857f69e6aab4f8becf768f346a510ae
Filippo Tessarotto <zoeslam@gmail.com>
2020-10-19 10:07:46 +0200

    Prefer an anonymous lambda for confined one-time execution

--- bin/phpunit-wrapper.php
+++ bin/phpunit-wrapper.php
@@ -2,49 +2,46 @@
 
 declare(strict_types=1);
 
-function phpunitWrapperMain(): void {
+use ParaTest\Runners\PHPUnit\Worker\WrapperWorker;
 
-  $opts = getopt('', [
-    'write-to:',
-  ]);
+(static function (): void {
+    $opts = getopt('', ['write-to:']);
 
-  $composerAutoloadFiles = [
-    dirname(__DIR__, 3) . DIRECTORY_SEPARATOR . 'autoload.php',
-    dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
-    dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
-  ];
+    $composerAutoloadFiles = [
+        dirname(__DIR__, 3) . DIRECTORY_SEPARATOR . 'autoload.php',
+        dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+        dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+    ];
 
-  foreach ($composerAutoloadFiles as $file) {
-    if (file_exists($file)) {
-      require_once $file;
-      define('PHPUNIT_COMPOSER_INSTALL', $file);
+    foreach ($composerAutoloadFiles as $file) {
+        if (file_exists($file)) {
+            require_once $file;
+            define('PHPUNIT_COMPOSER_INSTALL', $file);
 
-      break;
-    }
-  }
-
-  assert(is_string($opts['write-to']));
-  $writeTo = fopen($opts['write-to'], 'wb');
-  assert(is_resource($writeTo));
-
-  $i = 0;
-  while (true) {
-    $i++;
-    if (feof(\STDIN)) {
-      exit;
+            break;
+        }
     }
 
-    $command = fgets(\STDIN);
-    if ($command === false || $command === \ParaTest\Runners\PHPUnit\Worker\WrapperWorker::COMMAND_EXIT) {
-      exit;
+    assert(is_string($opts['write-to']));
+    $writeTo = fopen($opts['write-to'], 'wb');
+    assert(is_resource($writeTo));
+
+    $i = 0;
+    while (true) {
+        $i++;
+        if (feof(STDIN)) {
+            exit;
+        }
+
+        $command = fgets(STDIN);
+        if ($command === false || $command === WrapperWorker::COMMAND_EXIT) {
+            exit;
+        }
+
+        $arguments = unserialize($command);
+        (new PHPUnit\TextUI\Command())->run($arguments, false);
+
+        fwrite($writeTo, WrapperWorker::TEST_EXECUTED_MARKER);
+        fflush($writeTo);
     }
-
-    $arguments = unserialize($command);
-    (new PHPUnit\TextUI\Command)->run($arguments, false);
-
-    fwrite($writeTo, \ParaTest\Runners\PHPUnit\Worker\WrapperWorker::TEST_EXECUTED_MARKER);
-    fflush($writeTo);
-  }
-}
-
-phpunitWrapperMain();
+})();
