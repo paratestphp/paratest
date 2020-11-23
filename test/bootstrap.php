d5dcf15b813f798117a6de2c486fdb5c4313cbc3
Filippo Tessarotto <zoeslam@gmail.com>
2020-11-23 08:18:57 +0100

    Mmm

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,13 +2,5 @@
 
 declare(strict_types=1);
 
-set_error_handler(static function (int $errno, string $errstr = '', string $errfile = '', int $errline = 0): bool {
-    if ((error_reporting() & $errno) === 0) {
-        return true;
-    }
-
-    throw new ErrorException($errstr, $errno, $errno, $errfile, $errline);
-});
-
 require __DIR__ . DIRECTORY_SEPARATOR . 'constants.php';
 require PARATEST_ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
