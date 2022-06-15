3a4586ddc74c00fd240b4d4ea037209389f3bdb2
Filippo Tessarotto <zoeslam@gmail.com>
2022-06-15 11:42:56 +0200

    CS: disable mysterious Squiz.Functions.FunctionDeclaration.Found issue (#666)
    * CS: disable mysterious Squiz.Functions.FunctionDeclaration.Found issue
    
    * Test new config for confusing GA error
    
    * No .dist anymore
    
    * Howww to get you passing??
    
    * Let's see if a dep update on GA fix the unusual issue
    
    * Culprit found: hashFiles('**/composer.json') check for subdirectories, but one within the test folder is set with 0 perm by test

--- .github/lint-xml-configuration/lint-xml-configuration.sh
+++ .github/lint-xml-configuration/lint-xml-configuration.sh
@@ -4,7 +4,7 @@ set -ex
 
 phpunitXsd="vendor/phpunit/phpunit/phpunit.xsd"
 
-xmllint --noout --schema "$phpunitXsd" phpunit.xml.dist
-xmllint --noout --schema vendor/squizlabs/php_codesniffer/phpcs.xsd phpcs.xml.dist
-xmllint --noout --schema vendor/vimeo/psalm/config.xsd psalm.xml.dist
+xmllint --noout --schema "$phpunitXsd" phpunit.xml
+xmllint --noout --schema vendor/squizlabs/php_codesniffer/phpcs.xsd phpcs.xml
+xmllint --noout --schema vendor/vimeo/psalm/config.xsd psalm.xml
 find test/ -name "phpunit*.xml*" -print0 | xargs -0 xmllint --noout --schema "$phpunitXsd"
