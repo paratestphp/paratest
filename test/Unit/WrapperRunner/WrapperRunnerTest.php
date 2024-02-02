3297520be7ef1c222fee9ebd305d114c570e0c3a
Filippo Tessarotto <zoeslam@gmail.com>
2024-02-02 10:07:02 +0100

    Fix for OS related PHP versions

--- test/Unit/WrapperRunner/WrapperRunnerTest.php
+++ test/Unit/WrapperRunner/WrapperRunnerTest.php
@@ -253,7 +253,7 @@ final class WrapperRunnerTest extends TestBase
         self::assertNotFalse($format);
 
         $output = $result->output;
-        $output = preg_replace("/^Processes:     \\d+\nRuntime:       PHP \\d+.\\d+.\\d+(-dev)?\n\n/", '', $output, 1, $count);
+        $output = preg_replace("/^Processes:     \\d+\nRuntime:       PHP \\d+.\\d+.\\d+(-\w+)?\n\n/", '', $output, 1, $count);
         self::assertSame(1, $count);
         self::assertNotNull($output);
 
