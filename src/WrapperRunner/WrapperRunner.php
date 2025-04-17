872ab3fd9d0c38cfa384285d38a02eb267a4cc1e
Filippo Tessarotto <zoeslam@gmail.com>
2025-04-17 07:54:44 +0200

    Narrow actual types

--- src/WrapperRunner/WrapperRunner.php
+++ src/WrapperRunner/WrapperRunner.php
@@ -67,7 +67,7 @@ final class WrapperRunner implements RunnerInterface
     private array $teamcityFiles = [];
     /** @var list<SplFileInfo> */
     private array $testdoxFiles = [];
-    /** @var non-empty-string[] */
+    /** @var array<non-empty-string> */
     private readonly array $parameters;
     private CodeCoverageFilterRegistry $codeCoverageFilterRegistry;
 
@@ -84,8 +84,11 @@ final class WrapperRunner implements RunnerInterface
         $phpFinder = new PhpExecutableFinder();
         $phpBin    = $phpFinder->find(false);
         assert($phpBin !== false);
+        assert($phpBin !== '');
         $parameters = [$phpBin];
-        $parameters = array_merge($parameters, $phpFinder->findArguments());
+        /** @var array<non-empty-string> $arguments */
+        $arguments  = $phpFinder->findArguments();
+        $parameters = array_merge($parameters, $arguments);
 
         if ($options->passthruPhp !== null) {
             $parameters = array_merge($parameters, $options->passthruPhp);
