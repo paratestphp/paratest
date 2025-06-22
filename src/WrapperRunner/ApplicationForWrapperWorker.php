ce25b77fdb0a70a1d968f084107a96d5faf827d7
Filippo Tessarotto <zoeslam@gmail.com>
2025-06-22 18:26:18 +0200

    Update signature to match new upstream ones

--- src/WrapperRunner/ApplicationForWrapperWorker.php
+++ src/WrapperRunner/ApplicationForWrapperWorker.php
@@ -115,7 +115,7 @@ final class ApplicationForWrapperWorker
 
         $testSuite->run();
 
-        return TestResultFacade::result()->wasSuccessfulIgnoringPhpunitWarnings()
+        return TestResultFacade::result()->wasSuccessful()
             ? RunnerInterface::SUCCESS_EXIT
             : RunnerInterface::FAILURE_EXIT;
     }
