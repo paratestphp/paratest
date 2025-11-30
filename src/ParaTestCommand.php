97079b5503ebabd97a43385ba37ce0451adc440d
Niklas Grießer <niklas@griesser.me>
2025-11-30 09:02:33 +0100

    Add support for Symfony 8

--- src/ParaTestCommand.php
+++ src/ParaTestCommand.php
@@ -18,6 +18,7 @@ use function assert;
 use function class_exists;
 use function is_string;
 use function is_subclass_of;
+use function method_exists;
 use function sprintf;
 
 /** @internal */
@@ -45,7 +46,14 @@ final class ParaTestCommand extends Command
 
         $application->setName('ParaTest');
         $application->setVersion(PrettyVersions::getVersion('brianium/paratest')->getPrettyVersion());
-        $application->add($command);
+        // @phpstan-ignore function.alreadyNarrowedType (can be removed when dropping support for Symfony 7.3)
+        if (method_exists($application, 'addCommand')) {
+            $application->addCommand($command);
+        } else {
+            // @phpstan-ignore method.deprecated (can be removed when dropping support for Symfony 7.3)
+            $application->add($command);
+        }
+
         $commandName = $command->getName();
         assert($commandName !== null);
         $application->setDefaultCommand($commandName, true);
